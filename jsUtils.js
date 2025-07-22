18691881448ac06f0d7f83dc7f9ba21b1f39db4e
Georgiy Panyushkin <gpanyshkin@tourmalinecore.com>
2025-07-22 11:07:50 +0500

    cleanup: delete unused jsUtils

--- jsUtils.js
+++ jsUtils.js
@@ -1,22 +1,5 @@
 function fn() {
     return {
-        getAuthHeaders: function (tokenValue) {
-            return {
-                [this.getAuthHeaderKey()]: this.getAuthHeaderValue(tokenValue)
-            }
-        },
-
-        getAuthHeaderKey: function () {
-            return this.shouldUseFakeExternalDependencies()
-                ? 'X-DEBUG-TOKEN'
-                : 'Authorization';
-        },
-
-        getAuthHeaderValue: function (tokenValue) {
-            return this.shouldUseFakeExternalDependencies()
-                ? tokenValue
-                : 'Bearer ' + tokenValue;
-        },
 
         shouldUseFakeExternalDependencies: function () {
             return this.getEnvVariable('SHOULD_USE_FAKE_EXTERNAL_DEPENDENCIES') === 'true';            
