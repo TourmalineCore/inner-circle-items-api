4a8564e90c201f902d6c08a3dc7fa427d52f388c
Georgiy Panyushkin <gpanyshkin@tourmalinecore.com>
2025-07-24 10:52:34 +0500

    feat: create user permissions

---
+++ Api/UserClaimsProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using TourmalineCore.AspNetCore.JwtAuthentication.Core.Contract;
+
+namespace Api
+{
+    public class UserClaimsProvider : IUserClaimsProvider
+    {
+        public const string PermissionClaimType = "permissions";
+
+        public const string IsItemTypesHardDeleteAllowed = "IsItemTypesHardDeleteAllowed";
+        public const string CanManageItemsTypes = "CanManageItemsTypes";
+        public const string CanViewItemsTypes = "CanViewItemsTypes";
+
+        public Task<List<Claim>> GetUserClaimsAsync(string login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
