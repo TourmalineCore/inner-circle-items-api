842e361f339eac031a73fae8675c766145a53923
Yam1x <rychkov03@yandex.ru>
2025-07-25 11:38:08 +0500

    ci: add migration to configure relations between item and item types

---
+++ Application/Migrations/20250725063645_ItemItemTypeOneToManyRelation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemItemTypeOneToManyRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_ItemTypeId",
+                table: "Item",
+                column: "ItemTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Item_ItemTypes_ItemTypeId",
+                table: "Item",
+                column: "ItemTypeId",
+                principalTable: "ItemTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Item_ItemTypes_ItemTypeId",
+                table: "Item");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Item_ItemTypeId",
+                table: "Item");
+        }
+    }
+}
