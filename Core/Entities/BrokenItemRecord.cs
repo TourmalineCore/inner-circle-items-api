1d99c03f7cc5f19c14d0f980eb2387fc75229a1f
Yam1x <rychkov03@yandex.ru>
2025-07-25 10:58:02 +0500

    feat: add BrokenItemRecord, add isRemoved property for item and make s/n, description and holderId not required

---
+++ Core/Entities/BrokenItemRecord.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Entities
+{
+    public class BrokenItemRecord
+    {
+        public BrokenItemRecord() 
+        {
+
+        }
+        public long Id { get; set; }
+
+        public long ItemId { get; set; }
+        public Item Item { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly DateOfBroke { get; set; }
+
+        public DateOnly RepairDate {  get; set; }
+
+        public bool isFinished { get; set; } = false;
+    }
+}
--- Core/Entities/Item.cs
+++ Core/Entities/Item.cs
@@ -11,7 +11,7 @@
 
         public string Name { get; set; }
 
-        public string SerialNumber { get; set; }
+        public string? SerialNumber { get; set; }
 
         public long ItemTypeId { get; set; }
 
@@ -19,13 +19,16 @@
 
         public DateOnly PurchaseDate { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public long HolderId { get; set; }
+        public long? HolderId { get; set; }
 
-        public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; } = false;
 
-        public Status Status { get; set; }
+        public bool isRemoved { get; set; } = false;
+        public List<BrokenItemRecord> BrokenItemRecords { get; set; }
+
+        public Status Status { get; set; } = Status.ReadyToUse;
 
     }
 }
