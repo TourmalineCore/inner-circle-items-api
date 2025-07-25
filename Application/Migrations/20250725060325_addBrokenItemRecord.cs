64a0add24b01979cbf71653fad9a7d1b7b424c17
Yam1x <rychkov03@yandex.ru>
2025-07-25 11:04:49 +0500

    ci: update migrations with BrokenItemRecord table

---
+++ Application/Migrations/20250725060325_addBrokenItemRecord.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBrokenItemRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    SerialNumber = table.Column<string>(type: "text", nullable: true),
+                    ItemTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    PurchaseDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    HolderId = table.Column<long>(type: "bigint", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    isRemoved = table.Column<bool>(type: "boolean", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrokenItemRecord",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ItemId = table.Column<long>(type: "bigint", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    DateOfBroke = table.Column<DateOnly>(type: "date", nullable: false),
+                    RepairDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    isFinished = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrokenItemRecord", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BrokenItemRecord_Item_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Item",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrokenItemRecord_ItemId",
+                table: "BrokenItemRecord",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Item_Name",
+                table: "Item",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrokenItemRecord");
+
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
