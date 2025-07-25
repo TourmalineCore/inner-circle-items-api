086dd64ff3816a81dbcb9163aeb28a9ff8a54766
Yam1x <rychkov03@yandex.ru>
2025-07-25 11:01:39 +0500

    chore: configured mapping for BrokenItemRecord to use one-to-many relation

---
+++ Application/Mappings/BrokenItemRecordMapping.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Mappings;
+
+public class BrokenItemRecordMapping : IEntityTypeConfiguration<BrokenItemRecord>
+{
+    public void Configure(EntityTypeBuilder<BrokenItemRecord> builder)
+    {
+        builder
+            .Property(x => x.ItemId)
+            .IsRequired();
+
+        builder
+            .HasOne(e => e.Item)
+            .WithMany(e => e.BrokenItemRecords);
+    }
+}
