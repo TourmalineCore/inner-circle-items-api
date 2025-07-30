4cbd5c472ce99da15cd73746d582b73578015c8d
Yam1x <rychkov03@yandex.ru>
2025-07-30 15:52:03 +0500

    feat: add item and configure relations between item and item type

---
+++ Application/Mappings/ItemMapping.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Application.Mappings;
+
+public class ItemMaping : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder
+            .HasIndex(x => x.Id)
+            .IsUnique();
+
+        builder
+            .Property(e => e.TenantId)
+            .IsRequired();
+
+        builder
+            .Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(256);
+
+        builder
+            .Property(x => x.SerialNumber)
+            .HasMaxLength(128);
+
+        builder
+            .Property(x => x.ItemTypeId)
+            .IsRequired();
+
+        builder
+            .HasOne(x => x.ItemType)
+            .WithMany(x => x.Items)
+            .HasForeignKey(x => x.ItemTypeId);
+
+        builder
+            .Property(x => x.Price)
+            .IsRequired();
+    }
+}
---
+++ Core/Entities/Item.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.Entities
+{
+    public class Item
+    {
+        public Item ()
+        {
+
+        }
+
+        public long Id { get; set; }
+
+        public long TenantId { get; set; }
+
+        public string Name { get; set; }
+
+        public string SerialNumber { get; set; } = string.Empty;
+
+        public long ItemTypeId { get; set; }
+
+        public ItemType ItemType { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateOnly? PurchaseDate { get; set; }  
+
+        public long? HolderId { get; set; }
+    }
+}
