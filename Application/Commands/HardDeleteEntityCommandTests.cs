4d0163d60ecb3cfaad8cb1ef8f5c41368cab75e6
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-08-10 11:43:35 +0500

    test: should return true or false from HardDeleteEntityCommandTests.cs when it was deleted or not respectively

--- Application/Commands/HardDeleteEntityCommandTests.cs
+++ Application/Commands/HardDeleteEntityCommandTests.cs
@@ -27,16 +27,20 @@ public class HardDeleteEntityCommandTests
 
         var hardDeleteEntityCommand = new HardDeleteEntityCommand(appDbContext);
 
-        await hardDeleteEntityCommand.ExecuteAsync<Item>(1, 777);
+        var wasDeleted = await hardDeleteEntityCommand.ExecuteAsync<Item>(1, 777);
 
         var itemDoesNotExist = await appDbContext
             .Items
             .AllAsync(x => x.Id != 1);
 
+        Assert.True(wasDeleted);
         Assert.True(itemDoesNotExist);
 
+        var wasDeletedAgain = true;
+
         // try to delete again
-        Assert.Null(await Record.ExceptionAsync(() => hardDeleteEntityCommand.ExecuteAsync<Item>(1, 777)));
+        Assert.Null(await Record.ExceptionAsync(async () => wasDeletedAgain = await hardDeleteEntityCommand.ExecuteAsync<Item>(1, 777)));
+        Assert.False(wasDeletedAgain);
     }
 
     [Fact]
@@ -60,10 +64,16 @@ public class HardDeleteEntityCommandTests
 
         var hardDeleteEntityCommand = new HardDeleteEntityCommand(appDbContext);
 
+        var wasNonExistedDeleted = true;
+
         // try to delete a non-existent item from the same tenant
-        Assert.Null(await Record.ExceptionAsync(() => hardDeleteEntityCommand.ExecuteAsync<Item>(2, 777)));
+        Assert.Null(await Record.ExceptionAsync(async () => wasNonExistedDeleted = await hardDeleteEntityCommand.ExecuteAsync<Item>(2, 777)));
+        Assert.False(wasNonExistedDeleted);
+
+        var wasDeletedFromAnotherTenant = true;
 
         // try to delete an existent item from a wrong tenant
-        Assert.Null(await Record.ExceptionAsync(() => hardDeleteEntityCommand.ExecuteAsync<Item>(1, 888)));
+        Assert.Null(await Record.ExceptionAsync(async () => wasDeletedFromAnotherTenant = await hardDeleteEntityCommand.ExecuteAsync<Item>(1, 888)));
+        Assert.False(wasDeletedFromAnotherTenant);
     }
 }
