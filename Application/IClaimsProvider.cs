9ad6ea40c74b4b4d4333d9c523f4eb3dcaf68280
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-08-11 13:51:52 +0500

    docs: add motivation for IClaimsProvider as its summary docs

--- Application/IClaimsProvider.cs
+++ Application/IClaimsProvider.cs
@@ -1,6 +1,11 @@
 ﻿namespace Application;
 
-// https://stackoverflow.com/a/75203625
+
+/// <summary>
+/// we need a way to inject TenantId to the logic instead of passing it manually through all layers
+/// Api implements IClaimsProvider to provide it from a JWT token per each web request (scoped)
+/// actual implementation was inspired by this code snippet https://stackoverflow.com/a/75203625
+/// </summary>
 public interface IClaimsProvider
 {
     long TenantId { get; }
