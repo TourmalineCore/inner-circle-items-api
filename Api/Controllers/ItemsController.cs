75f1c95d0814ef62975fab344df7a14de7c4967e
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-11-18 21:49:34 +0500

    format: revert nesting change from ItemsController to make PR cleaner

--- Api/Controllers/ItemsController.cs
+++ Api/Controllers/ItemsController.cs
@@ -9,93 +9,95 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TourmalineCore.AspNetCore.JwtAuthentication.Core.Filters;
 
-namespace Api.Controllers;
-
-[Authorize]
-[ApiController]
-[Route("items")]
-public class ItemsController : ControllerBase
+namespace Api.Controllers
 {
-    /// <summary>
-    ///     Get all items
-    /// </summary>
-    [RequiresPermission(UserClaimsProvider.CanViewItems)]
-    [HttpGet]
-    public async Task<ItemsResponse> GetAllItemsAsync(
-        [FromServices] AllItemsQuery allItemsQuery,
-        [FromServices] EmployeesApi employeesApi
-    )
+
+    [Authorize]
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
     {
-        var items = await allItemsQuery.GetAsync();
-
-        var allEmployeesResponse = await employeesApi.GetAllEmployeesAsync();
-
-        return new ItemsResponse
+        /// <summary>
+        ///     Get all items
+        /// </summary>
+        [RequiresPermission(UserClaimsProvider.CanViewItems)]
+        [HttpGet]
+        public async Task<ItemsResponse> GetAllItemsAsync(
+            [FromServices] AllItemsQuery allItemsQuery,
+            [FromServices] EmployeesApi employeesApi
+        )
         {
-            Items = items
-                .Select(x => new ItemDto
-                {
-                    Id = x.Id,
-                    Name = x.Name,
-                    SerialNumber = x.SerialNumber,
-                    ItemType = new ItemTypeDto
+            var items = await allItemsQuery.GetAsync();
+
+            var allEmployeesResponse = await employeesApi.GetAllEmployeesAsync();
+
+            return new ItemsResponse
+            {
+                Items = items
+                    .Select(x => new ItemDto
                     {
-                        Id = x.ItemType.Id,
-                        Name = x.ItemType.Name
-                    },
-                    Price = x.Price,
-                    Description = x.Description,
-                    PurchaseDate = x.PurchaseDate,
-                    HolderEmployee = HolderEmployeeMapper.MapToEmployeeDto(x.HolderEmployeeId, allEmployeesResponse)
-                })
-                .ToList()
-        };
-    }
+                        Id = x.Id,
+                        Name = x.Name,
+                        SerialNumber = x.SerialNumber,
+                        ItemType = new ItemTypeDto
+                        {
+                            Id = x.ItemType.Id,
+                            Name = x.ItemType.Name
+                        },
+                        Price = x.Price,
+                        Description = x.Description,
+                        PurchaseDate = x.PurchaseDate,
+                        HolderEmployee = HolderEmployeeMapper.MapToEmployeeDto(x.HolderEmployeeId, allEmployeesResponse)
+                    })
+                    .ToList()
+            };
+        }
 
-    /// <summary>
-    ///     Add an item
-    /// </summary>
-    /// <param name="createItemRequest"></param>
-    [RequiresPermission(UserClaimsProvider.CanManageItems)]
-    [HttpPost]
-    public async Task<CreateItemResponse> CreateItemAsync(
-        [FromServices] CreateItemCommand createItemCommand,
-        [Required][FromBody] CreateItemRequest createItemRequest
-    )
-    {
-        var createItemCommandParams = new CreateItemCommandParams
+        /// <summary>
+        ///     Add an item
+        /// </summary>
+        /// <param name="createItemRequest"></param>
+        [RequiresPermission(UserClaimsProvider.CanManageItems)]
+        [HttpPost]
+        public async Task<CreateItemResponse> CreateItemAsync(
+            [FromServices] CreateItemCommand createItemCommand,
+            [Required][FromBody] CreateItemRequest createItemRequest
+        )
         {
-            Name = createItemRequest.Name,
-            SerialNumber = createItemRequest.SerialNumber,
-            ItemTypeId = createItemRequest.ItemTypeId,
-            Price = createItemRequest.Price,
-            Description = createItemRequest.Description,
-            PurchaseDate = createItemRequest.PurchaseDate,
-            HolderEmployeeId = createItemRequest.HolderEmployeeId
-        };
+            var createItemCommandParams = new CreateItemCommandParams
+            {
+                Name = createItemRequest.Name,
+                SerialNumber = createItemRequest.SerialNumber,
+                ItemTypeId = createItemRequest.ItemTypeId,
+                Price = createItemRequest.Price,
+                Description = createItemRequest.Description,
+                PurchaseDate = createItemRequest.PurchaseDate,
+                HolderEmployeeId = createItemRequest.HolderEmployeeId
+            };
 
-        var newItemId = await createItemCommand.ExecuteAsync(createItemCommandParams);
+            var newItemId = await createItemCommand.ExecuteAsync(createItemCommandParams);
 
-        return new CreateItemResponse()
+            return new CreateItemResponse()
+            {
+                NewItemId = newItemId
+            };
+        }
+
+        /// <summary>
+        ///     Deletes specific item
+        /// </summary>
+        /// <param name="itemId"></param>
+        [RequiresPermission(UserClaimsProvider.AUTO_TESTS_ONLY_IsItemsHardDeleteAllowed)]
+        [HttpDelete("{itemId}/hard-delete")]
+        public async Task<object> HardDeleteItemAsync(
+            [FromServices] HardDeleteItemCommand hardDeleteItemCommand,
+            [Required][FromRoute] long itemId
+        )
         {
-            NewItemId = newItemId
-        };
-    }
-
-    /// <summary>
-    ///     Deletes specific item
-    /// </summary>
-    /// <param name="itemId"></param>
-    [RequiresPermission(UserClaimsProvider.AUTO_TESTS_ONLY_IsItemsHardDeleteAllowed)]
-    [HttpDelete("{itemId}/hard-delete")]
-    public async Task<object> HardDeleteItemAsync(
-        [FromServices] HardDeleteItemCommand hardDeleteItemCommand,
-        [Required][FromRoute] long itemId
-    )
-    {
-        return new
-        {
-            isDeleted = await hardDeleteItemCommand.ExecuteAsync(itemId)
-        };
+            return new
+            {
+                isDeleted = await hardDeleteItemCommand.ExecuteAsync(itemId)
+            };
+        }
     }
 }
