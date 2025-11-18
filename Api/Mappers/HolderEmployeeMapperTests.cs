696a712fc3b4841f4057a70344b771eda56fe2ab
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-11-18 17:07:07 +0500

    test: add HolderEmployeeMapperTests.cs to tests Not Found employee fullName case that is not covered with e2e

---
+++ Api/Mappers/HolderEmployeeMapperTests.cs
@@ -0,0 +1,23 @@
+using Api.EnternalDeps.EmployeesApi.Responses;
+using Xunit;
+
+namespace Api.Mappers;
+
+public class HolderEmployeeMapperTests
+{
+    [Fact]
+    public void MapNonExistentEmployee_ShouldUseNotFoundAsFullName()
+    {
+        var empltyEmployeesResponse = new EmployeesResponse
+        {
+            Employees = new List<EmployeesResponse.EmployeeDto>()
+        };
+
+        var employeeId = 5;
+
+        var mappedHolderEmployeeDto = HolderEmployeeMapper.MapToEmployeeDto(employeeId, empltyEmployeesResponse);
+
+        Assert.Equal(employeeId, mappedHolderEmployeeDto!.Id);
+        Assert.Equal(HolderEmployeeMapper.NotFoundEmployeeFullName, mappedHolderEmployeeDto.FullName);
+    }
+}
