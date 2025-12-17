37b74e8ff47bb1f28d5c0f8a894823f10e8707c0
Aleksandr Shinkarev <ashinkarev@tourmalinecore.com>
2025-12-17 19:19:58 +0500

    Revert "feat!: target master branch"
    This reverts commit f837950010c58531724bf1969b054e84964b8f4c.

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   // You can find out more about the configuration of this file here https://semantic-release.gitbook.io/semantic-release/usage/configuration
   branches: [
-    'master'
+    'feature/add-semantic-release'
   ],
   // Plugins https://semantic-release.gitbook.io/semantic-release/extending/plugins-list
   plugins: [
